3cd2c9657e73f7ce2aa0cb29a3a9aaba04247d47
Victor LANCIEN <vlancien@e3r5p8.42.fr>
2017-01-07 21:22:16 +0100

    First upload

---
+++ data.js
@@ -0,0 +1,39 @@
+const data = [
+	{
+		"id": "1",
+		"name": "Viko",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "2",
+		"name": "Salut",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "3",
+		"name": "bonjour",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "4",
+		"name": "cC",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "5",
+		"name": "QQER",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "6",
+		"name": "LOL",
+		"email": "Salut@live.fr"
+	},
+	{
+		"id": "7",
+		"name": "AOKE",
+		"email": "Salut@live.fr"
+	}
+];
+
+module.exports = data;
