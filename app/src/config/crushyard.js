f5386b8a356bb4a901c2ac2e5e6b28463f199b99
Viko@raspberry <victor@stationf.co>
2017-10-14 19:53:08 +0200

    Few correct

--- app/src/config/crushyard.js
+++ app/src/config/crushyard.js
@@ -1,3 +1,9 @@
 exports.logoName	= "CrushYard";
-exports.apiUrl		= "http://127.0.0.1:8080"
-exports.urlApp		= "http://localhost:3000"
+
+if (process.env.NODE_ENV === "development") {
+	exports.apiUrl		= "http://127.0.0.1:8080"
+	exports.urlApp		= "http://localhost:3000"
+} else {
+	exports.apiUrl		= "http://79.137.36.192:46008"
+	exports.urlApp		= "http://79.137.36.192:46009"
+}
--- app/src/config/services.js
+++ app/src/config/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const apiUrl = "http://localhost:8080";
+const {apiUrl} = require('./crushyard');
 
 const tabs = {
 
