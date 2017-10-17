50a0be86b4171fc0c74e856000fa13f941772343
Viko@raspberry <victor@stationf.co>
2017-10-18 00:36:04 +0200

    ip

--- app/src/config/crushyard.js
+++ app/src/config/crushyard.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV === "development") {
 	exports.apiUrl		= "http://127.0.0.1:8080"
 	exports.urlApp		= "http://localhost:5000"
 } else {
-	exports.apiUrl		= "http://79.137.36.192:46008"
-	exports.urlApp		= "http://79.137.36.192:46009"
+	exports.apiUrl		= "http://79.137.36.192:"+process.env.PORTAPI
+	exports.urlApp		= "http://79.137.36.192"+process.env.PORTAPP
 }
