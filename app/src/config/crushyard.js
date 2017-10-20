d0d0ed898481c7e716f5c6441b097838e6cfb891
Viko@raspberry <victor@stationf.co>
2017-10-20 19:42:03 +0200

    Ip

--- app/src/config/crushyard.js
+++ app/src/config/crushyard.js
@@ -1,9 +1,10 @@
 exports.logoName	= "CrushYard";
 
 if (process.env.NODE_ENV === "development") {
-	exports.apiUrl		= "http://e1r12p13.42.fr:8080"
-	// exports.apiUrl		= "http://127.0.0.1:8080"
-	exports.urlApp		= "http://e1r12p13.42.fr:5000"
+	// exports.apiUrl		= "http://e1r12p13.42.fr:8080"
+	exports.apiUrl		= "http://127.0.0.1:8080"
+	exports.urlApp		= "http://127.0.0.1:5000"
+	// exports.urlApp		= "http://e1r12p13.42.fr:5000"
 } else {
 	exports.apiUrl		= "https://79.137.36.192:"+process.env.PORTAPI
 	exports.urlApp		= "https://79.137.36.192"+process.env.PORTAPP
